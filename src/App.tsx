4d909323503b7d0152f7693708babe5d365ed307
otinomz <awesomeotinomz@gmail.com>
2022-07-06 19:46:47 +0100

    feat: extension completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import {VocabList} from "./packages/vocabList"
 
 function App() {
+  const randomNumber = Math.floor(Math.random() * Math.floor(VocabList.length))
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Exhale Words</p>
+      <a className="email" href="https://mail.google.com/mail/u/0/#inbox">Email</a>
+      <a className="twitter" href="https://twitter.com/">Twitter</a>
+      <a className="whatsApp" href="https://web.whatsapp.com/">WhatsApp</a>
+      <a className="youtube" href="https://youtube.com/">YouTube</a>
+      <a className="github" href="https://github.com/">Github</a>
+      <a className="localhost" href="http://localhost:3000/">localhost:3000</a>
+      <a className="localhost2" href="http://localhost:3001/">localhost:3001</a>
+
+      <h1>{VocabList[randomNumber].word}</h1>
+      <h3>{VocabList[randomNumber].definition }.</h3>
     </div>
-  );
+
+  )
 }
 
 export default App;
