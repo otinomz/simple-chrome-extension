84b63473b6a51caf2c5ffd4e16241e86867af8ce
otinomz <awesomeotinomz@gmail.com>
2022-07-07 11:53:21 +0100

    chore: code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.scss'
+import { dailyLink } from './packages/dailyLink';
 import {VocabList} from "./packages/vocabList"
 
 function App() {
@@ -9,16 +10,13 @@ function App() {
     <div className="App">
       <p>Exhale Words</p>
       <a className="portfolio" href="https://otinomz.vercel.app">My Portfolio</a>
-      <a className="email" href="https://mail.google.com/mail/u/0/#inbox">Email</a>
-      <a className="figma" href= "https://www.figma.com/"> Figma </a>
-      <a className="slack" href="https://app.slack.com/"> Slack </a>
-      <a className="twitter" href="https://twitter.com/">Twitter</a>
-      <a className="whatsApp" href="https://web.whatsapp.com/">WhatsApp</a>
-      <a className="youtube" href="https://youtube.com/">YouTube</a>
-      <a className="github" href="https://github.com/">Github</a>
-      <a className="localhost" href="http://localhost:3000/">localhost:3000</a>
-      <a className="localhost2" href="http://localhost:3001/">localhost:3001</a>
-
+      
+      <div className="frequentLink">
+        {dailyLink.map((dayLink) => (
+          <a key={dayLink.id} href={dayLink.link}>{dayLink.name}</a>    
+        ))}
+      </div>
+      
       <h1>{VocabList[randomNumber].word}</h1>
       <h3>{VocabList[randomNumber].definition}.</h3>
         
---
+++ src/packages/dailyLink.ts
@@ -0,0 +1,55 @@
+export const dailyLink = [
+    {
+      id: "#1",
+      link: "https://mail.google.com/mail/u/0/#inbox",
+      name: "Gmail"
+    },
+
+    {
+      id: "#2",
+      link: "https://www.figma.com/",
+      name: "Figma"
+    },
+
+    {
+      id: "#3",
+      link: "https://app.slack.com/",
+      name: "Slack"
+    },
+    
+    {
+      id: "#4",
+      link: "https://twitter.com/",
+      name: "Twitter"
+    },
+    
+    {
+      id: "#5",
+      link: "https://web.whatsapp.com/",
+      name: "WhatsApp"
+    },
+    
+    {
+      id: "#6",
+      link: "https://youtube.com/",
+      name: "YouTube"
+    },
+    
+    {
+      id: "#7",
+      link: "https://github.com/",
+      name: "Github"
+    },
+    
+    {
+      id: "#8",
+      link: "http://localhost:3000/",
+      name: "Localhost:3000"
+    },
+    
+    {
+      id: "#9",
+      link: "http://localhost:3001/",
+      name: "Localhost:3001"
+    },
+]
